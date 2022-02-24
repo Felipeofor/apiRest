5752cd50ed5b284e921d8ac28e54da407d6647c0
Ramos Felipe <felipeofor@gmail.com>
2022-02-24 13:33:42 -0300

    api funcional

--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ routes.get('/', (req, res)=>{
         if(err) return res.send(err)
 
         conn.query('SELECT * FROM ahorro', (err, rows)=>{
+            console.log(err, "err get")
             if(err) return res.send(err)
 
             res.json(rows)
@@ -21,7 +22,6 @@ routes.get('/:id', (req, res)=>{
 
         conn.query('SELECT * FROM ahorro WHERE id = ?', [id], (err, rows)=>{
             if(err) return res.send(err)
-
             res.json(rows)
         })
     })
@@ -30,11 +30,12 @@ routes.get('/:id', (req, res)=>{
 
 routes.post('/', (req, res)=>{
     req.getConnection((err, conn)=>{
+        console.log(req.body, 'req.body')
         if(err) return res.send(err)
         conn.query('INSERT INTO ahorro set ?', [req.body], (err, rows)=>{
+            console.log(err, 'errorNull?')
             if(err) return res.send(err)
-
-            res.send('ahorro add')
+            res.send(rows)
         })
     })
 })
@@ -43,6 +44,7 @@ routes.delete('/:id', (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
         conn.query('DELETE FROM ahorro WHERE id = ?', [req.params.id], (err, rows)=>{
+            console.log(err, 'err delete')
             if(err) return res.send(err)
 
             res.send('ahorro excluded!')
